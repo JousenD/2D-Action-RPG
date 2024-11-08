979a145f702a8ad39ebfaa69ddfcdeeb3f9e72c4
JousenD <j.romero.lopera@gmail.com>
2024-11-08 17:54:44 +0100

    Player Animations and Movement Implemented

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,19 +1,74 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
+using UnityEngine.XR;
 
 public class Player : MonoBehaviour
 {
     private Rigidbody2D rb;
-    // Start is called before the first frame update
+    private Animator anim;
+
+   [SerializeField] private float moveSpeed;
+   [SerializeField] private float jumpForce; 
+   private float xInput;
+
+   private int facingDir = 1;
+   private bool facingRight = true;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponentInChildren<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        CheckInput();
+        Movement();
+        FLipController();
+        AnimatorControllers();
+    }
+
+    private void CheckInput()
+    {
+        xInput = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+    }
+
+    private void Movement()
+    {
+        rb.velocity = new Vector2(xInput * moveSpeed, rb.velocity.y);
+    }
+
+    private void Jump()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+    }
+
+    private void AnimatorControllers()
+    {
+        bool isMoving = rb.velocity.x != 0;
+
+        anim.SetBool("isMoving", isMoving);
+    }
+
+    private void Flip()
+    {
+        facingDir = facingDir*-1;
+        facingRight = !facingRight;
+        transform.Rotate(0,180,0);
+    }
+
+    private void FLipController()
+    {
+        if(rb.velocity.x > 0 && !facingRight)
+            Flip();
+        else if (rb.velocity.x < 0 && facingRight)
+            Flip();
     }
 }
