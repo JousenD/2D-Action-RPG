0e6df5519561a1523b0fdceac44deb7cae7f5612
JousenD <j.romero.lopera@gmail.com>
2024-11-15 19:59:42 +0100

    Attack Mechanic Implemented

--- Assets/PlayerIdleState.cs
+++ Assets/PlayerIdleState.cs
@@ -14,8 +14,7 @@ public class PlayerIdleState : PlayerGroundedState
     {
         base.Enter();
 
-        rb.velocity = new Vector2(0,0);
-    }
+        player.ZeroVelocity();    }
 
     public override void Exit()
     {
@@ -30,7 +29,7 @@ public class PlayerIdleState : PlayerGroundedState
             return;
 
 
-        if(xInput != 0)
+        if(xInput != 0 && !player.isBusy)
             stateMachine.ChangeState(player.moveState);
     }
 }
