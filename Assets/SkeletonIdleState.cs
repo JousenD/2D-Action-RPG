3417fc8cc436742e9eaa4f23402f89aeb75e7bcb
JousenD <j.romero.lopera@gmail.com>
2024-11-17 01:44:36 +0100

    Enemy State Machine - Idle and Move

---
+++ Assets/SkeletonIdleState.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class SkeletonIdleState : EnemyState
+{
+    private EnemySkeleton enemy;
+    public SkeletonIdleState(Enemy _enemyBase, EnemyStateMachine _statemachine, string _animBoolName, EnemySkeleton _enemy) : base(_enemyBase, _statemachine, _animBoolName)
+    {
+        this.enemy = _enemy;
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+
+        stateTimer = enemy.idleTime;
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+    }
+
+    public override void Update()
+    {
+        base.Update();
+
+        if (stateTimer < 0)
+            stateMachine.ChangeState(enemy.moveState);
+    }
+}
